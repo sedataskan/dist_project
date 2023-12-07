d5aedc563712d19cc388a4fe5858ebe5be23b2e0
sedataskan <sedanurgfb6671@hotmail.com>
2023-12-07 11:00:24 +0300

    added security dependencies and one new endpoint

--- src/main/java/dist_system/movie_app/controller/MovieController.java
+++ src/main/java/dist_system/movie_app/controller/MovieController.java
@@ -6,9 +6,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/movie")
 public class MovieController {
 
+        // get movie by id with param
         @GetMapping("/{id}")
         public String getMovieById(@PathVariable String id) {
             return "Movie with id: " + id;
+        }
+
+        // get all movies
+        @GetMapping("/all")
+        public String getAllMovies() {
+            return "All movies";
+        }
+
+        // get ratings for movie with id
+        @GetMapping("/{id}/ratings")
+        public String getRatingsForMovie(@PathVariable String id) {
+            return "Ratings for movie with id: " + id;
         }
 
         // for adding movie rating
@@ -23,24 +36,16 @@ public class MovieController {
             return "Movie added";
         }
 
-        // for deleting movie
-        @DeleteMapping("/delete")
-        public String deleteMovie() {
-            return "Movie deleted";
-        }
-
         // for adding review
         @PostMapping("/add/review")
         public String addReview() {
             return "Review added";
         }
 
-        // get all movies
-        @GetMapping("/all")
-        public String getAllMovies() {
-            return "All movies";
+        // for deleting movie
+        @DeleteMapping("/delete")
+        public String deleteMovie() {
+            return "Movie deleted";
         }
-
-        // get movie by id with param
 
 }
