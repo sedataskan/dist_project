21b623ad417be2782b420c2459c9c24cf7539e7a
sedataskan <sedanurgfb6671@hotmail.com>
2023-12-07 14:00:48 +0300

    splitted logic code to Service

--- src/main/java/dist_system/movie_app/controller/MovieController.java
+++ src/main/java/dist_system/movie_app/controller/MovieController.java
@@ -1,50 +1,25 @@
 package dist_system.movie_app.controller;
 
-import org.json.JSONArray;
+import dist_system.movie_app.service.MovieService;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
-    //get apikey from application.properties
-    @Value("${api.key}")
-    private String apiKey;
+
+    //created movieService object to use its methods in controller with constructor injection
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
     // get movie by id with param
     @GetMapping("/{id}")
     public String getMovieById(@PathVariable String id) throws IOException, InterruptedException, JSONException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.themoviedb.org/3/keyword/"+ id +"/movies"))
-                .header("accept", "application/json")
-                .header("Authorization", "Bearer " + apiKey)
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-
-        // Parse JSON response
-        JSONObject jsonResponse = new JSONObject(response.body());
-
-        // Extract movie information
-        JSONArray resultsArray = jsonResponse.getJSONArray("results");
-        if (resultsArray.length() > 0) {
-            JSONObject firstResult = resultsArray.getJSONObject(0);
-            String movieTitle = firstResult.getString("original_title");
-            String movieInfo = firstResult.getString("overview");
-            System.out.println("Got movie by this id: " + id);
-            return movieTitle + "\n" + movieInfo;
-        } else {
-            return "Movie not found";
-        }
+        return movieService.getMovieById(id);
     }
 
     // get all movies
